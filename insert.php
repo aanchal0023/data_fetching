c22b3c8ccbb639cfeb85e85072ffb3511eb5adb7
aanchal0023 <48906902+aanchal0023@users.noreply.github.com>
2020-10-11 20:32:44 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -11,26 +11,47 @@ $result=mysqli_query($mysqli,"SELECT * FROM formdata ORDER BY id DESC");
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+<style>
+	.container{
+	
+	}
+	.container-fluid{
+		background-color: orange;
+		width: 100%;	
+		}
+   		
+    .column{
+    	float: left;
+        width: 35%;
+    }
+    .row{
+    	width: 100%;
+    }
+</style>
 <body>
 	<form action="function.php" method="POST">
 		<h3 style="text-align: center;"> Data Form</h3><br>
-		<table border="0" width="100%;">
+		<div class="row">
+			<div class="column"></div>
+			<table border="0">
+		
 			<tbody><tr>
-				<td style="text-align:center;"><b>Name:</b></td></tr>
-		<tr><td style="text-align: center;"><input type="text" name="name" placeholder="Name"><br></td></tr>
-		<tr><td style="text-align: center;"><b>City:</b></td></tr>
-		<tr><td style="text-align: center;"><input type="" name="city" placeholder="City"><br></td></tr>
-		<tr><td style="text-align: center;"><b>Occupation:</b></td></tr>
-		<tr><td style="text-align: center;"><input type="" name="occupation" placeholder="Occupation"><br></td></tr>	
+				
+				<td><b>Name:</b></td></tr>
+		<tr><td><input type="text" name="name" placeholder="Name"><br></td></tr>
+		<tr><td><b>City:</b></td></tr>
+		<tr><td><input type="" name="city" placeholder="City"><br></td></tr>
+		<tr><td><b>Occupation:</b></td></tr>
+		<tr><td><input type="" name="occupation" placeholder="Occupation"><br></td></tr>	
 		<tr>
 			<td></td></tr>
-			<tr><td style="text-align: center;"><input type="submit" name="submit" style="background-color:#00cc00; color: white;"></td></tr>
-	</form>
-		<div class="container">
+			<tr><td><input type="submit" name="submit" style="background-color:#00cc00; color: white;"></td></tr>
+	</form></tbody></table></div>
+	<div class="container">
 	<table border="0"><br>
 				<h4 style="text-align: center; background-color:#000099; color: white;">Result Data</h4>
 		<tr>
-			<th style="width:375px;"> Name</th>
+			<th style="width:375px;" > Name</th>
 			<th style="width:375px;">City</th>
 			<th style="width:375px;">Occupation</th>
 		</tr>
@@ -45,7 +66,7 @@ while($res=mysqli_fetch_array($result)){
 	echo '</tr>';
 }
 ?>
-			</table></div>
+</table></div>
 
 </body>
 </html>
